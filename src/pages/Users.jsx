95f4e5fc0cf1434e0659e68a228b890b3d3d73b1
AbhishekCS3459 <abhishekverman3459@gmail.com>
2024-03-12 00:42:46 +0530

    changed user

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -18,7 +18,7 @@ const Users = () => {
   };
 
   const sendRequest = async () => {
-    const res = await axios.get("http://localhost:5000/api/user", {
+    const res = await axios.get("/api/user", {
       withCredentials: true,
     });
     const data = await res.data;
